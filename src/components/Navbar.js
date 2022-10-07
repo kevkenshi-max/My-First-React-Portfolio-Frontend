5e0b8b85be658f24791f99f0b453b1d106ce67a3
Kevin Lee <kevin-lee@lambdastudents.comgit config --global user.email kevkenshi@gmail.com>
2022-10-07 00:28:29 -0700

    kevin-lee

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,22 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { FaBars, FaTimes } from 'react-icons/fa'
 import Logo from '../images/logo.gif'
 import './Navbar.css'
 
 const Navbar = () => {
+    const [click, setClick] = useState(false)
+
+    const handleClick = () => setClick(!click)
+
     return (
         <div className='navbar'>
             <div className='logo'>
                 <img src={Logo} alt='logo' />
             </div>
-            <ul className='nav-menu'>
+            <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                 <li className='nav-item'><a href='/'>Home</a></li>
                 <li className='nav-item'><a href='/'>About</a></li>
                 <li className='nav-item'><a href='/'>Faq</a></li>
                 <li className='nav-item'><a href='/'>Contact</a></li>
             </ul>
-            <div className='hamburger'>
-                <FaBars />
+            <div className='hamburger' onClick={handleClick}>
+                {click ? (<FaTimes size={30} />) : (<FaBars size={30} />)}
+                
             </div>
         </div>
     )
